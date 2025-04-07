f9e4f438b8e60eed06dfa7421ca49f5a411e03b7
Angela Navarro Sala <angnavar@c3r12s6.42madrid.com>
2025-04-07 17:28:51 +0200

    !

--- Pipex/src/pipex.c
+++ Pipex/src/pipex.c
@@ -6,7 +6,7 @@
 /*   By: angnavar <angnavar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/03 02:28:22 by angnavar          #+#    #+#             */
-/*   Updated: 2025/02/04 10:48:39 by angnavar         ###   ########.fr       */
+/*   Updated: 2025/02/12 16:54:11 by angnavar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -58,28 +58,30 @@ void	execute_command(char *cmd, char **envp)
 	}
 }
 
-void	set_childs(t_pipex *px, char **argv, char **envp)
+void	set_childs(t_pipex *pipex, char **argv, char **envp)
 {
-	px->childs[0] = fork();
-	if (px->childs[0] == 0)
+	pipex->childs[0] = fork();
+	if (pipex->childs[0] == 0)
 	{
-		dup2(px->file[0], STDIN_FILENO);
-		dup2(px->pipe_fd[1], STDOUT_FILENO);
-		close(px->pipe_fd[0]);
+		dup2(pipex->file[0], STDIN_FILENO);
+		dup2(pipex->pipe_fd[1], STDOUT_FILENO);
+		close(pipex->pipe_fd[0]);
 		execute_command(argv[2], envp);
 	}
-	px->childs[1] = fork();
-	if (px->childs[1] == 0)
+	pipex->childs[1] = fork();
+	if (pipex->childs[1] == 0)
 	{
-		dup2(px->pipe_fd[0], STDIN_FILENO);
-		if (px->file[1] >= 0)
-			dup2(px->file[1], STDOUT_FILENO);
+		dup2(pipex->pipe_fd[0], STDIN_FILENO);
+		if (pipex->file[1] >= 0)
+			dup2(pipex->file[1], STDOUT_FILENO);
 		else
 		{
-			perror(argv[4]);
+			write(2, "zsh: permission denied: ", 25);
+			write(2, argv[4], ft_strlen(argv[4]));
+			write(2, "\n", 1);
 			exit(1);
 		}
-		close(px->pipe_fd[1]);
+		close(pipex->pipe_fd[1]);
 		execute_command(argv[3], envp);
 	}
 }
@@ -89,7 +91,9 @@ void	open_files(int file[2], char **argv, int pipe_fd[2])
 	file[0] = open(argv[1], O_RDONLY);
 	if (file[0] < 0)
 	{
-		perror(argv[1]);
+		write(2, "zsh: permission denied: ", 25);
+		write(2, argv[1], ft_strlen(argv[1]));
+		write(2, "\n", 1);
 		file[0] = open("/dev/null", O_RDONLY);
 		if (file[0] < 0)
 			exit(1);
@@ -104,18 +108,18 @@ void	open_files(int file[2], char **argv, int pipe_fd[2])
 
 void	pipex(char **argv, char **envp)
 {
-	t_pipex	px;
+	t_pipex	pipex;
 	int		status[2];
 
-	open_files(px.file, argv, px.pipe_fd);
-	set_childs(&px, argv, envp);
-	close(px.pipe_fd[0]);
-	close(px.pipe_fd[1]);
-	close(px.file[0]);
-	if (px.file[1] >= 0)
-		close(px.file[1]);
-	waitpid(px.childs[0], &status[0], 0);
-	waitpid(px.childs[1], &status[1], 0);
+	open_files(pipex.file, argv, pipex.pipe_fd);
+	set_childs(&pipex, argv, envp);
+	close(pipex.pipe_fd[0]);
+	close(pipex.pipe_fd[1]);
+	close(pipex.file[0]);
+	if (pipex.file[1] >= 0)
+		close(pipex.file[1]);
+	waitpid(pipex.childs[0], &status[0], 0);
+	waitpid(pipex.childs[1], &status[1], 0);
 	if (WIFEXITED(status[1]))
 		exit(WEXITSTATUS(status[1]));
 	else
--- Pipex/src_bonus/main_bonus.c
+++ Pipex/src_bonus/main_bonus.c
@@ -1,33 +1,38 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   main.c                                             :+:      :+:    :+:   */
+/*   main_bonus.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: angnavar <angnavar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/31 14:30:20 by angnavar          #+#    #+#             */
-/*   Updated: 2025/02/12 14:48:49 by angnavar         ###   ########.fr       */
+/*   Updated: 2025/02/13 18:18:32 by angnavar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "../includes/pipex.h"
+#include "../includes/pipex_bonus.h"
 
 int	main(int argc, char **argv, char **envp)
 {
 	int	i;
+	int	here_doc_mode;
 
-	if (argc != 5)
+	if (argc < 5)
 	{
-		write(2, "Usage: ./pipex infile cmd1 cmd2 outfile\n", 40);
+		write(2, "Usage: ./pipex_bonus infile cmd1 cmd2 ... outfile\n", 50);
 		return (1);
 	}
 	i = 0;
-	while (i < 5)
+	while (i < argc)
 	{
 		if (ft_strlen(argv[i]) < 1)
 			return (0);
 		i++;
 	}
-	pipex(argv, envp);
+	here_doc_mode = !ft_strcmp(argv[1], "here_doc");
+	if (here_doc_mode)
+		do_here_doc(argc, argv, envp);
+	else
+		pipex(argv, envp, argc - 3, 0);
 	return (0);
 }
