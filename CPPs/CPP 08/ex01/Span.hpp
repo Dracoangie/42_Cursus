fc05174368441cb833558ce9048f1908f4290668
DevPatata <anavaro.sala@gmail.com>
2026-05-20 11:32:15 +0200

    Cpp 08 exo01

--- CPPs/CPP 08/ex01/Span.hpp
+++ CPPs/CPP 08/ex01/Span.hpp
@@ -1,29 +1,43 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   easyfind.hpp                                       :+:      :+:    :+:   */
+/*   Span.hpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: angnavar <angnavar@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/05/19 13:25:04 by angnavar          #+#    #+#             */
-/*   Updated: 2026/05/20 10:56:25 by angnavar         ###   ########.fr       */
+/*   Updated: 2026/05/20 11:30:45 by angnavar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef EASYFIND_HPP
-#define EASYFIND_HPP
+#ifndef SPAN_HPP
+#define SPAN_HPP
 
 #include <algorithm>
-#include <iterator>
 #include <stdexcept>
-
-template <typename T>
-typename T::iterator easyfind(T& container, int value)
+#include <vector>
+class Span
 {
-	typename T::iterator it = std::find(container.begin(), container.end(), value);
-	if (it == container.end())
-		throw std::runtime_error("Value not found");
-	return it;
-}
+	std::vector<int> numbers;
+	unsigned int maxSize;
+public:
+	Span(unsigned int n);
+	Span(const Span& other);
+	Span& operator=(const Span& other);
+	~Span();
+	
+	void addNumber(int num);
+	int shortestSpan() const;
+	int longestSpan() const;
+
+	template <typename Iter>
+	void addRange(Iter begin, Iter end)
+	{
+		if (numbers.size() + std::distance(begin, end) > maxSize)
+			throw std::runtime_error("Span is full");
+		numbers.insert(numbers.end(), begin, end);
+	}
+};
+
 
 #endif
--- CPPs/CPP 08/ex01/main.cpp
+++ CPPs/CPP 08/ex01/main.cpp
@@ -6,31 +6,92 @@
 /*   By: angnavar <angnavar@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/13 20:41:40 by angnavar          #+#    #+#             */
-/*   Updated: 2026/05/20 10:57:54 by angnavar         ###   ########.fr       */
+/*   Updated: 2026/05/20 11:31:37 by angnavar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "Span.hpp"
 #include <iostream>
-#include "easyfind.hpp"
 #include <vector>
+#include <cstdlib>
+#include <ctime>
 
-int main(void)
+int main()
 {
-	std::vector<int> vec;
-	vec.push_back(1);
-	vec.push_back(2);
-	vec.push_back(3);
-	try
-	{		
-		std::vector<int>::iterator it = easyfind(vec, 2);
-		std::cout << "Value found: " << *it << std::endl;
-	}
-	catch (const std::exception& e) { std::cerr << e.what() << std::endl; }
-	try
-	{		
-		std::vector<int>::iterator it = easyfind(vec, 4);
-		std::cout << "Value found: " << *it << std::endl;
-	}
-	catch (const std::exception& e) { std::cerr << e.what() << std::endl; }
+    try
+    {
+        // Test del ejemplo del subject
+        std::cout << "=== Subject test ===" << std::endl;
+
+        Span sp(5);
+
+        sp.addNumber(6);
+        sp.addNumber(3);
+        sp.addNumber(17);
+        sp.addNumber(9);
+        sp.addNumber(11);
+
+        std::cout << "Shortest: " << sp.shortestSpan() << std::endl;
+        std::cout << "Longest : " << sp.longestSpan() << std::endl;
+
+
+        std::cout << "\n=== Large test ===" << std::endl;
+
+        const int SIZE = 10000;
+
+        Span big(SIZE);
+
+        std::srand(std::time(NULL));
+
+        std::vector<int> numbers;
+
+        for (int i = 0; i < SIZE; i++)
+            numbers.push_back(std::rand());
+
+        // usar el método por rango
+        big.addRange(numbers.begin(), numbers.end());
+
+        std::cout << "Stored: " << SIZE << " numbers" << std::endl;
+        std::cout << "Shortest: " << big.shortestSpan() << std::endl;
+        std::cout << "Longest : " << big.longestSpan() << std::endl;
+
+
+        std::cout << "\n=== Exception test ===" << std::endl;
+
+        try
+        {
+            big.addNumber(42);
+        }
+        catch(const std::exception& e)
+        {
+            std::cout << "Expected exception: "
+                      << e.what() << std::endl;
+        }
+
+
+        std::cout << "\n=== Not enough numbers ===" << std::endl;
+
+        try
+        {
+            Span test(5);
+
+            test.addNumber(1);
+
+            std::cout << test.shortestSpan() << std::endl;
+        }
+        catch(const std::exception& e)
+        {
+            std::cout << "Expected exception: "
+                      << e.what() << std::endl;
+        }
+    }
+    catch(const std::exception& e)
+    {
+        std::cerr << "Error: "
+                  << e.what()
+                  << std::endl;
+    }
+
     return 0;
 }
+
