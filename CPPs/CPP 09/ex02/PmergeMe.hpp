fd0499270ec1e314b8090537e9c2189c2295e2d3
DevPatata <anavaro.sala@gmail.com>
2026-05-24 23:04:37 +0200

    cpp09 ex02 [WIP]

--- CPPs/CPP 09/ex02/PmergeMe.hpp
+++ CPPs/CPP 09/ex02/PmergeMe.hpp
@@ -1,22 +1,36 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   main.cpp                                           :+:      :+:    :+:   */
+/*   PmergeMe.hpp                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: angnavar <angnavar@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/10/13 20:41:40 by angnavar          #+#    #+#             */
-/*   Updated: 2026/05/24 21:12:31 by angnavar         ###   ########.fr       */
+/*   Created: 2026/05/20 11:33:47 by angnavar          #+#    #+#             */
+/*   Updated: 2026/05/24 23:03:50 by angnavar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef PMERGEME_HPP
+#define PMERGEME_HPP
 
-
-#include <iostream>
+#include <stack>
+#include <iterator>
 #include <vector>
+#include <list>
 
-int main(void)
+class PmergeMe
 {
+	std::vector<int>	vec;
+	std::list<int>		list;
 
-}
+public:
+	PmergeMe();
+	PmergeMe(const PmergeMe& other);
+	PmergeMe &operator=(const PmergeMe& other);
+	~PmergeMe();
 
+	void vecOrder(std::vector<int>	other);
+	void lsitOrder(std::list<int>	other);
+};
+
+#endif
